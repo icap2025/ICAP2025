e923486aeb91b7882e5ec4d52b8711d9bcba5cad
robin <rirobink@gmail.com>
2025-05-09 14:45:18 +0600

    landing  page created

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -12,16 +11,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "ICPS 2025",
   description: "International Conference on Physical Science 2025",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
